b075269512bb823270f3174a34c027d42759cb89
Bonnie <jose.vlima321@gmail.com>
2024-09-10 18:43:34 -0300

    Atualizando projeto

---
+++ projeto-javaspring-claro/src/vic/claro/exemplo/App.java
@@ -0,0 +1,10 @@
+package vic.claro.exemplo;
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        String primeiroNome = "José";
+
+        System.out.println(primeiroNome);
+        
+    }
+}
